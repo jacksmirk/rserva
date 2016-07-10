bb997a3cb1030dae371fb86ab1208937ff756976
jacksmirk <jacksmirk@gmail.com>
2016-07-10 14:04:00 +0200

    feature (server): add Bootstrap and home

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars');
 
-app.get('/', function(req, res) {
-	res.send('Hello, world!');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('index', {
+    	name: 'Jack'
+    });
 });
 
-app.use('/public', express.static('public'));
+app.use('/static', express.static('public'));
+app.get('/home', function(req, res) {
+	res.render('home');
+});
 
-app.listen(3000);
+app.listen(3000, function() {
+	console.log('Server up: http://localhost:3000');
+});
